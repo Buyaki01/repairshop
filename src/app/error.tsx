39143d2f65a457881251364c1d809f36792f3775
ritta.sweta <ritta.sweta@savannahinformatics.com>
2025-01-29 17:36:19 +0300

    Configure error files for Sentry

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import * as Sentry from "@sentry/nextjs";
+import { useEffect } from "react";
+
+const error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  useEffect(() => {
+    console.error(error);
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+};
+
+export default error;
